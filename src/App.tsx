31e1a96551d89b4f69b486263c14f3322b5d831a
MikeLaptop <micheledong2803@gmail.com>
2025-02-13 19:38:49 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+      <Square/>
+    </>
+  )
+}
+
+function Square() {
+  return <button className="square">X</button>;
+}
