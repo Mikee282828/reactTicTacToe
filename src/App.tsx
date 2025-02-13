a62597e6cb2633586c34aea01cd069a5e959464b
MikeLaptop <micheledong2803@gmail.com>
2025-02-13 23:58:49 +0100

    props passing value

--- src/App.tsx
+++ src/App.tsx
@@ -14,26 +14,24 @@ function Board() {
   return (
     <>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
       </div>
       <div className="board-row">
-      <Square/>
-      <Square/>
-      <Square/>
-
+        <Square value={1} />
+        <Square value={1} />
+        <Square value={1} />
       </div>
       <div className="board-row">
-      <Square/>
-      <Square/>
-      <Square/>
-
+        <Square value={1} />
+        <Square value={1} />
+        <Square value={1} />
       </div>
     </>
   );
 }
 
-function Square(){
-  return <button className="square">1</button>;
+function Square({ value }: any) {
+  return <button className="square">{value}</button>;
 }
