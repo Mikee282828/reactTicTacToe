a354435a60791f4079d5d21c978ca72ecb714c79
MikeLaptop <micheledong2803@gmail.com>
2025-02-16 23:40:15 +0100

    time traveling

--- src/App.tsx
+++ src/App.tsx
@@ -13,15 +13,19 @@ export default function App() {
 function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [xIsNext, setXIsNext] = useState(true);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextBoard:any){
-    setHistory([...history,nextBoard]);   // ...history is for all history items
+    const nextHistory = [...history.slice(0,currentMove+1),nextBoard];
+    setHistory(nextHistory);   // ...history is for all history items
+    setCurrentMove(nextHistory.length-1);
     setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove:number){
-
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 == 0);
   }
 
   const moves = history.map((nextBoard,move) => {
